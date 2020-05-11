9c847c9b6c57e3ff49b403730b5f9adf7c7f66be
Pankaj Jangid <pankaj.jangid>
2020-05-11 15:13:35 +0530

    Add project files.

---
+++ CleanArch.Mvc/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArch.Mvc.Data
+{
+    public class UniversityIdentityDbContext : IdentityDbContext
+    {
+        public UniversityIdentityDbContext(DbContextOptions<UniversityIdentityDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
