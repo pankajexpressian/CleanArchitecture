42374594a42be84284ddda32b4ee6740b27ca278
Pankaj Jangid <pankaj.jangid>
2020-05-11 15:36:09 +0530

    Complete implementation of service and repository

---
+++ CleanArch.Infra.Data/Repository/CourseRepository.cs
@@ -0,0 +1,22 @@
+﻿using CleanArch.Domain.Interfaces;
+using CleanArch.Domain.Models;
+using CleanArch.Infra.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Infra.Data.Repository
+{
+    public class CourseRepository : ICourseRepository
+    {
+        private UniversityDbContext _context;
+        public CourseRepository(UniversityDbContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<Course> GetCourses()
+        {
+            return _context.Courses;
+        }
+    }
+}
