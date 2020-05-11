0f453abc0f42ca85b1143ca096f0f7c637d3e247
Pankaj Jangid <pankaj.jangid>
2020-05-11 16:08:09 +0530

    Created course controller in the presentation and added the dependency. Project architecture without CQRS is completed here.

---
+++ CleanArch.Mvc/Controllers/CoursesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CleanArch.Application.Interfaces;
+
+namespace CleanArch.Mvc.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly ICourseService _courseService;
+
+        public CoursesController(ICourseService courseService)
+        {
+
+            _courseService = courseService;
+        }
+
+        // GET: Courses
+        public IActionResult Index()
+        {
+
+            return View(_courseService.GetCourses());
+        }
+    }
+}
